0980039bf7f894f1e2cc708530a95ab2298a3d5b
udukunda1 <139108920+udukunda1@users.noreply.github.com>
2023-09-07 16:29:33 +0200

    Patron.cxx

--- Patron.cxx
+++ Patron.cxx
@@ -1,121 +1,183 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
+using namespace std;
 
-class Book {
-private:
-    std::string ISBN;
-    std::string title;
-    std::string author;
-    int copyrightDate;
-    bool checkedOut;
-    std::string genre;
-
-public:
-    Book(std::string isbn, std::string t, std::string a, int cd, std::string g)
-        : ISBN(isbn), title(t), author(a), copyrightDate(cd), genre(g), checkedOut(false) {}
-
-    std::string getISBN() const { return ISBN; }
-    std::string getTitle() const { return title; }
-    std::string getAuthor() const { return author; }
-    bool isCheckedOut() const { return checkedOut; }
-    void checkOut() { checkedOut = true; }
-    void checkIn() { checkedOut = false; }
-    std::string getGenre() const { return genre; }
-
-    bool operator==(const Book& other) const { return ISBN == other.ISBN; }
-    bool operator!=(const Book& other) const { return !(*this == other); }
-
-    friend std::ostream& operator<<(std::ostream& os, const Book& book);
+//enumerator to hold possible genres
+enum Genre {
+    Fiction,
+    NonFiction,
+    Periodical,
+    Biography,
+    Children
 };
 
-std::ostream& operator<<(std::ostream& os, const Book& book) {
-    os << "Title: " << book.title << "\nAuthor: " << book.author
-       << "\nISBN: " << book.ISBN << "\nGenre: " << book.genre;
-    return os;
-}
-
-class Patron {
+//Define the class Book
+class Book {
 private:
-    std::string userName;
-    std::string cardNumber;
-    int owedFees;
+    string ISBN;
+    string title;
+    string author;
+    int copyrightDate;
+    bool checkedOut;
+    Genre genre;
 
 public:
-    Patron(std::string name, std::string card)
-        : userName(name), cardNumber(card), owedFees(0) {}
+    // Constructors
+    Book(const string& isbn, const string& title, const string& author, int copyrightDate, Genre genre)
+        : ISBN(isbn), title(title), author(author), copyrightDate(copyrightDate), checkedOut(false), genre(genre) {}
 
-    std::string getUserName() const { return userName; }
-    std::string getCardNumber() const { return cardNumber; }
-    int getOwedFees() const { return owedFees; }
-    bool owesFees() const { return owedFees > 0; }
-    void setFees(int fees) { owedFees = fees; }
+    // Getter functions
+    string getISBN() const { return ISBN; }
+    string getTitle() const { return title; }
+    string getAuthor() const { return author; }
+    int getCopyrightDate() const { return copyrightDate; }
+    bool isCheckedOut() const { return checkedOut; }
+    Genre getGenre() const { return genre; }
+
+    // Check out a book
+    void checkOut() { checkedOut = true; }
+
+    // Check in a book
+    void checkIn() { checkedOut = false; }
+
+     // Overload the == operator
+    bool operator==(const Book& other) const {
+        return ISBN == other.ISBN;
+    }
+
+    // Overload the != operator
+    bool operator!=(const Book& other) const {
+        return !(*this == other);
+    }
+
+    // Overload the << operator for output
+    friend ostream& operator<<(ostream& os, const Book& book) {
+        os << "Title: " << book.title << "\nAuthor: " << book.author << "\nISBN: " << book.ISBN << "\nGenre: ";
+        switch (book.getGenre()) {
+        case Fiction: os << "Fiction"; break;
+        case NonFiction: os << "Non-Fiction"; break;
+        case Periodical: os << "Periodical"; break;
+        case Biography: os << "Biography"; break;
+        case Children: os << "Children"; break;
+        default: os <<"Invalid genre";
+    };
+        return os;
+    }
+};
+
+//Define the class Patron
+class Patron {
+    private://private attributes
+        string userNname;
+        string cardNumber;
+        int ownedFees;
+    public:
+        //constructor
+        Patron(string u, string c, int f) : userNname(u), cardNumber(c), ownedFees(f) {}
+        
+        //getter functions
+        string getUserName() {return userNname;}
+        string getCardNumber() {return cardNumber;}
+        int getOwnedFees() {return ownedFees;}
+
+        //owesFees figures out if the patron owes fees
+        bool owesFees() {
+            if(ownedFees > 0) {return true;}
+            else{return false;}
+        }
+
+        //setFees sets fees for the patron
+        void setFees(int f) {
+            if(f >= 0) {
+                ownedFees = f;
+            }
+        }
+        //Overloading the == operator to be able to compare patron objects
+        bool operator==(const Patron& other) const {
+            return cardNumber == other.cardNumber;
+        }
 };
 
 class Transaction {
-private:
-    Book book;
-    Patron patron;
-    std::string activity;
-    std::string date;
+    private://private attributes
+        Book book;
+        Patron patron;
+        string activity;
+        string date;
+    public:
+        //constructor
+        Transaction(Book& b, Patron& p, string a, string d) : book(b), patron(p), activity(a), date(d) {}
 
-public:
-    Transaction(const Book& b, const Patron& p, std::string act, std::string d)
-        : book(b), patron(p), activity(act), date(d) {}
 };
-
+// Defining the Libary class
 class Library {
-private:
-    std::vector<Book> books;
-    std::vector<Patron> patrons;
-    std::vector<Transaction> transactions;
-
-public:
-    void addBook(const Book& book) { books.push_back(book); }
-
-    void checkOutBook(const Book& book, const Patron& patron, std::string date) {
-        if (std::find(books.begin(), books.end(), book) == books.end()) {
-            std::cout << "Error: Book not found in the library." << std::endl;
-            return;
+    private://private attributes
+        vector<Book> books;
+        vector<Patron> patrons;
+        vector<Transaction> transactions;
+        vector<Patron> debtors;
+    public:
+        //Definition of the addBook() function
+        void addBook(Book& book) {
+            books.push_back(book);
+        }
+        //Definition of the addPatron() function
+        void addPatron(Patron& p) {
+            patrons.push_back(p);
         }
 
-        if (std::find(patrons.begin(), patrons.end(), patron) == patrons.end()) {
-            std::cout << "Error: Patron not found in the library." << std::endl;
-            return;
+        //debtorsFinder() finds debtor patrons in the patrons vector
+        vector<Patron> debtorsFinder() {
+            for(auto i : patrons) {
+                if(i.getOwnedFees() > 0) {
+                    debtors.push_back(i);
+                }
+            }
+            return debtors;
         }
+        
+        //CheckOut function allows to check out books if patron is registered and owes no fees
+        void checkOut(Patron& patron, Book& book, string activity, string date) {
+            debtorsFinder();// find debtors
 
-        if (patron.owesFees()) {
-            std::cout << "Error: Patron owes fees." << std::endl;
-            return;
-        }
+            auto bookIter = find(books.begin(), books.end(), book);
+            auto patronIter = find(patrons.begin(), patrons.end(), patron);
 
-        book.checkOut();
-        transactions.push_back(Transaction(book, patron, "Check Out", date));
-    }
-
-    void displayPatronsWithFees() {
-        for (const Patron& patron : patrons) {
-            if (patron.owesFees()) {
-                std::cout << "Patron with fees: " << patron.getUserName() << std::endl;
+            //Is patron and book registered?
+            if(patronIter == patrons.end() || bookIter == books.end()) {
+                cout<<"Error the user or the book is not registered."<<endl;
+            }else if(find(debtors.begin(), debtors.end(), patron) != debtors.end()) { //Does debtor owe fees?
+                cout<<"The user owes fees."<<endl;
+            }else {//Otherwise record the transaction.
+                Transaction tran1(book, patron, activity, date);
+                transactions.push_back(tran1);
+                cout<<"Activity recorded"<<endl;
             }
         }
-    }
+        
 };
 
 int main() {
-    // Create instances of Book, Patron, and Library
-    Book book("123456789", "The Catcher in the Rye", "J.D. Salinger", 1951, "Fiction");
-    Patron patron("Alice Smith", "P12345");
-    Library library;
+    //Testing Code...
+    Book book1("978-0-12-345678-9", "Sample Book 1", "Author 1", 2023, Fiction);
+    Book book2("978-9-87-654321-0", "Sample Book 2", "Author 2", 2022, Biography);
+    Patron person1("trial", "0281918", 0);
+    Library tran;
 
-    // Add book to library
-    library.addBook(book);
+    tran.addBook(book1);
+    tran.addBook(book2);
+    tran.addPatron(person1);
+    tran.checkOut(person1, book1, "checkOut", "0/0/2023");
 
-    // Check out the book
-    library.checkOutBook(book, patron, "2023-08-15");
+    cout << "Book 1:\n" << book1 <<endl<<endl;
+    cout << "Book 2:\n" << book2 <<endl<<endl;
 
-    // Display patrons with fees
-    library.displayPatronsWithFees();
+    if (book1 == book2) {
+        cout << "Book 1 and Book 2 have the same ISBN." <<endl;
+    } else {
+        cout << "Book 1 and Book 2 have different ISBNs." <<endl;
+    }
 
     return 0;
 }
